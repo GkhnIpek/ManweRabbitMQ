acd0aa67d526ce3429ebab43a585e0fa315deee5
GÖKHAN İPEK <gokipek91@gmail.com>
2020-05-11 01:36:26 +0300

    default exchange

--- ManweRabbitMQ/ManweRabbitMQ.Consumer/Program.cs
+++ ManweRabbitMQ/ManweRabbitMQ.Consumer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
 
 namespace ManweRabbitMQ.Consumer
 {
@@ -6,7 +10,51 @@ namespace ManweRabbitMQ.Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory(); //Rabbitmq servisleri ile bağlantı kurmak için oluşturulur.
+            factory.Uri = new Uri("amqp://uiibvwim:YkWdsj19hOXbf1WbY79Ts9dn77zlka-5@orangutan.rmq.cloudamqp.com/uiibvwim");
+            //factory.HostName = "localhost"; //Local rabbitmq servisine bağlanma için
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //queue = kuyruk ismimiz
+                    //durable = eğer false olursa bizim rabbitmq instance'mız restart atarsa bu kuyruk silinir çünkü bellekte tutulur.Ancak true olursa rabbitmq bunu bir fiziksel diske yazar ve kaybolmaz.
+                    //exclusive = bu kuyruğa bir tane mi kanal bağlansın yoksa birden fazla kanal bağlansın mı bunu belirtmek için kullanılır.True olursa tek kanal bağlanır.
+                    //autoDelete = eğer kuyrukta bulunan son mesaj da bu kuyruktan çıkarsa bu kuyruk silinsin mi belirtmek için.True olursa otomatik olarak silinir.
+                    channel.QueueDeclare("task_queue", true, false, false, null);
+
+                    //prefetchCount: aynı anda kaç mesajın verilmek istendiği.
+                    //global: true dersek kaç tane instance varsa tüm instance lar toplam prefetchCount kadar alabilir.Eğer false olursa her instance prefetchCount kadar mesaj alabilir.
+                    channel.BasicQos(0, 1, false);
+
+                    Console.WriteLine("Mesajları bekliyorum...");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    //autoAck = true olursa mesaj kuyruktan silinir işi biter bitmez.
+                    channel.BasicConsume("task_queue", autoAck: false, consumer);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        Console.WriteLine("Mesaj alındı: " + message);
+                        int time = int.Parse(GetMessage(args));
+                        Thread.Sleep(time);
+                        Console.WriteLine("Mesaj işlendi.");
+
+                        //autoAck konusunda false dediğimiz için(autoack false mesajın silme işlemi yapılmıyor otomatikman) burada mesajın başarılı bir şekilde işlendi mesajı silebilirsin demek istedik.
+                        channel.BasicAck(ea.DeliveryTag, multiple: false);
+                    };
+                    Console.WriteLine("Çıkış yapmak için tıklayınız.");
+                    Console.ReadLine();
+                }                
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args[0].ToString();
         }
     }
 }
--- ManweRabbitMQ/ManweRabbitMQ.Publisher/Program.cs
+++ ManweRabbitMQ/ManweRabbitMQ.Publisher/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace ManweRabbitMQ.Publisher
 {
@@ -6,8 +8,42 @@ namespace ManweRabbitMQ.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World2!");
+            var factory = new ConnectionFactory(); //Rabbitmq servisleri ile bağlantı kurmak için oluşturulur.
+            factory.Uri = new Uri("amqp://uiibvwim:YkWdsj19hOXbf1WbY79Ts9dn77zlka-5@orangutan.rmq.cloudamqp.com/uiibvwim");
+            //factory.HostName = "localhost"; //Local rabbitmq servisine bağlanma için
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //queue = kuyruk ismimiz
+                    //durable = eğer false olursa bizim rabbitmq instance'mız restart atarsa bu kuyruk silinir çünkü bellekte tutulur.Ancak true olursa rabbitmq bunu bir fiziksel diske yazar ve kaybolmaz.
+                    //exclusive = bu kuyruğa bir tane mi kanal bağlansın yoksa birden fazla kanal bağlansın mı bunu belirtmek için kullanılır.True olursa tek kanal bağlanır.
+                    //autoDelete = eğer kuyrukta bulunan son mesaj da bu kuyruktan çıkarsa bu kuyruk silinsin mi belirtmek için.True olursa otomatik olarak silinir.
+                    channel.QueueDeclare("task_queue", durable: true, false, false, null);
+
+                    string message = GetMessage(args);
+                    for (int i = 1; i < 11; i++)
+                    {
+                        var bodyByte = Encoding.UTF8.GetBytes($"{message}-{i}");
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true; //mesajı korumak için, silinmesini engellemek için kullanılır. Yukardaki kuyruğu sağlama aldığımız gibi(durable:false)
+
+
+                        channel.BasicPublish("", routingKey: "task_queue", properties, body: bodyByte);
+                        Console.WriteLine($"Mesajınız gönderilmiştir: {message}-{i}");
+                    }
+
+                }
+
+                Console.WriteLine("Çıkış yapmak için tıklayınız.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args[0].ToString();
         }
     }
 }
