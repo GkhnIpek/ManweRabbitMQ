8a7579295ff3d2ae6761969fc70dce5ced54f561
GÖKHAN İPEK <gokipek91@gmail.com>
2020-05-10 18:13:45 +0300

    first commit

---
+++ ManweRabbitMQ/ManweRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManweRabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ManweRabbitMQ/ManweRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ManweRabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
